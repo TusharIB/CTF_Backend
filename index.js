f38d5b9de4c327aa293c5079606f20562b10fe53
Tushar-rao <tusharyadav5424@gmail.com>
2023-08-18 11:43:32 +0530

    ssl_changed

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7070;
 
 app.listen(
   PORT,
