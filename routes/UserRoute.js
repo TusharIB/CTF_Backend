fb70daf6544cbded23527b737c7c27a3904b5447
Tushar-rao <tusharyadav5424@gmail.com>
2023-08-18 13:06:15 +0530

    file_structure_changed

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,5 +1,23 @@
 const express = require('express');
-const User = require('../models/UserModel.js');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  main_address: String,
+  created: { type: Date, default: Date.now },
+  total_wins: { type: Number, default: 0 },
+  points: { type: Number, default: 0 },
+  played_Events: [
+    {
+      event_id: String,
+      name: String,
+      result: { type: Boolean, default: false },
+    },
+  ],
+});
+
+const User = mongoose.model('User', userSchema);
+
 
 const router = express.Router();
 
