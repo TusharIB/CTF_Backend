6029372eaa8a5330e0378b98c29847bcf2bb2c35
Tushar-rao <tusharyadav5424@gmail.com>
2023-08-18 12:11:34 +0530

    src_folder_added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 import path from "path";
 
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import cors from "cors";
 import router from "./routes/UserRoute.js";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 connectDB();
--- src/routes/GamesRoute.js
+++ src/routes/GamesRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Game from '../models/GameModel.js';
+import Game from '../../models/GameModel.js';
 
 const router = express.Router();
 
