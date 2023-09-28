4539f2fd1af9cf90e2ddfddfb21861c9e06cd333
TusharIB <104901533+TusharIB@users.noreply.github.com>
2023-09-28 12:38:56 +0530

    Update UserRoute.js

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,22 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  main_address: String,
-  created: { type: Date, default: Date.now },
-  total_wins: { type: Number, default: 0 },
-  points: { type: Number, default: 0 },
-  played_Events: [
-    {
-      event_id: String,
-      name: String,
-      result: { type: Boolean, default: false },
-    },
-  ],
-});
-
-const User = mongoose.model('User', userSchema);
+const User = require('../models/UserModel');
 
 
 const router = express.Router();
